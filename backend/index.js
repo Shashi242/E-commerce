f72b7117898f5a71a10c098058468953f4879cd9
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2023-06-06 22:23:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const Stripe = require('stripe')
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "100mb" }));
+app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
 
